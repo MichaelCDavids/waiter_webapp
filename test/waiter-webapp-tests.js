fca8dac2d6017a9f484885b7a15e7c560b202529
michael <michaelcdavids@gmail.com>
2018-09-07 16:40:45 +0200

    adding context to tests

--- test/waiter-webapp-tests.js
+++ test/waiter-webapp-tests.js
@@ -3,20 +3,34 @@ const assert = require('assert');
 const WaiterFactory = require('../waiter-factory');
 const pg = require('pg');
 const Pool = pg.Pool;
-const connectionString = process.env.DATABASE_URL || 'postgresql://postgres@localhost:5432/waiter_availability_test';
+const connectionString = process.env.DATABASE_URL || 'postgresql://coder:pg123@localhost:5432/waiter_availability_test';
 const pool = new Pool({
     connectionString
 });
 
-describe('The addRegistrationNumber function', function () {
+describe('The Waiter Availability Webapp Functions', function () {
     beforeEach(async function () {
-        await pool.query('');
+        await pool.query('delete from shifts');
     });
-    it('', async function () {
+
+    it('should allow a waiter to choose days that he/she can work on', async function () {
         let waiterFactoryObject = WaiterFactory(pool);
-        let variable = await waiterFactoryObject.function();
-        assert.strictEqual(variable, '');
+        let variable = await waiterFactoryObject.chooseDays();
+        assert.strictEqual(variable, 'This function should set days a waiter is available');
     });
+
+    // it('should clear shifts for one week', async function () {
+    //     let waiterFactoryObject = WaiterFactory(pool);
+    //     let variable = await waiterFactoryObject.function();
+    //     assert.strictEqual(variable, '');
+    // });
+
+    // it('should', async function () {
+    //     let waiterFactoryObject = WaiterFactory(pool);
+    //     let variable = await waiterFactoryObject.function();
+    //     assert.strictEqual(variable, '');
+    // });
+
     after(async function () {
         await pool.end();
     });
