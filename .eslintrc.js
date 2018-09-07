9f45ccb138f45e1e69070aebab7f78e06c212022
michael <michaelcdavids@gmail.com>
2018-09-07 16:38:47 +0200

    made beforeEach and after globals

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,9 @@ module.exports = {
     "globals": {
         "describe" : true,
         "it" : true,
-        "require" : true
-    }
+        "require" : true,
+        "beforeEach" : true,
+        "after" : true
+    },
+
 };
