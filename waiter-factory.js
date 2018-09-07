72b71c7af8c2313267f5ddf6a3936e72d8e79c11
michael <michaelcdavids@gmail.com>
2018-09-07 16:42:10 +0200

    adding functions

--- waiter-factory.js
+++ waiter-factory.js
@@ -1,3 +1,15 @@
-module.exports = function (pool){
-    
+module.exports = function (pool) {
+    async function setDays () {
+        return 'This function should set days a waiter is available';
+    };
+
+    async function clearDays () {
+        await pool.query('delete * from table3');
+    }
+
+    return {
+        chooseDays: setDays,
+        reset: clearDays
+    };
 }
+;
