254555b05f494debc003f921da78b57eeea7010b
michael <michaelcdavids@gmail.com>
2018-09-10 16:59:43 +0200

    removed '/' route

--- waiter-routes.js
+++ waiter-routes.js
@@ -1,9 +1,31 @@
 module.exports = function (waiterFactory) {
-    async function index (req, res) {
-        res.render('index');
+
+    async function waiters (req, res) {
+        let name = req.params.username;
+        let data = {
+            days: await waiterFactory.days(),
+            shift: await waiterFactory.shift(name)
+        };
+        res.render('waiter', { data });
+    };
+
+    async function bookShift (req, res) {
+        let name = req.params.username;
+        let shift = req.body.days;
+        await waiterFactory.schedule(name, shift);
+        let data = {
+            days: await waiterFactory.days(),
+            shift: await waiterFactory.shift(name)
+        };
+        res.render('waiter', { data });
+    };
+    async function admin (req, res) {
+        res.render('admin');
     };
 
     return {
-        index
+        waiters,
+        bookShift,
+        admin
     };
 };
