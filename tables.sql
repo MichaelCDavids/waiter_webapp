70a339f7b6a9c643026abb3b417054f2b4c64862
michael <michaelcdavids@gmail.com>
2018-09-10 16:56:16 +0200

    redefined database schema

--- tables.sql
+++ tables.sql
@@ -3,37 +3,36 @@ drop table if exists staff,weekdays,shifts;
 create table staff
 (
     id serial primary key not null,
-    first_name text not null,
-    position text not null
+    first_name text not null
 );
 
 create table weekdays
 (
     id serial primary key not null,
-    week_days text not null
+    day_name text not null
 );
 
-create table shifts
-(
+
+create table shifts(
     id serial primary key not null,
     waiter_id int not null,
-    shift_id int not null,
+    weekday_id int not null,
     foreign key (waiter_id) references staff(id),
-    foreign key (shift_id) references weekdays(id)
+    foreign key (weekday_id) references weekdays(id)
 );
 
-INSERT into staff (first_name, position) values ('Michael','Owner');
-INSERT into staff (first_name, position) values ('Unalo','Waiter');
-INSERT into staff (first_name, position) values ('Vusi','Waiter');
-INSERT into staff (first_name, position) values ('Schtoo','Waiter');
-INSERT into staff (first_name, position) values ('Pumlani','Waiter');
-INSERT into staff (first_name, position) values ('Lwando','Waiter');
-INSERT into staff (first_name, position) values ('Sibabalwe','Waiter');
+INSERT into staff (first_name) values ('Michael');
+INSERT into staff (first_name) values ('Unalo');
+INSERT into staff (first_name) values ('Vusi');
+INSERT into staff (first_name) values ('Schtoo');
+INSERT into staff (first_name) values ('Pumlani');
+INSERT into staff (first_name) values ('Lwando');
+INSERT into staff (first_name) values ('Sibabalwe');
 
-INSERT into weekdays (weekdays) values ('Sunday');
-INSERT into weekdays (weekdays) values ('Monday');
-INSERT into weekdays (weekdays) values ('Tuesday');
-INSERT into weekdays (weekdays) values ('Wednesday');
-INSERT into weekdays (weekdays) values ('Thursday');
-INSERT into weekdays (weekdays) values ('Friday');
-INSERT into weekdays (weekdays) values ('Saturday');
+INSERT into weekdays (day_name) values ('Sunday');
+INSERT into weekdays (day_name) values ('Monday');
+INSERT into weekdays (day_name) values ('Tuesday');
+INSERT into weekdays (day_name) values ('Wednesday');
+INSERT into weekdays (day_name) values ('Thursday');
+INSERT into weekdays (day_name) values ('Friday');
+INSERT into weekdays (day_name) values ('Saturday');
